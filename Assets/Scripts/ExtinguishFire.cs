3239f68d638488b1582a80aaf42887ea00b45298
Dasani <virej@liverpool.ac.uk>
2024-11-06 12:20:50 +0000

    feat: remote config for cloud settings added

--- Assets/Scripts/ExtinguishFire.cs
+++ Assets/Scripts/ExtinguishFire.cs
@@ -1,17 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Services.RemoteConfig;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 
 public class ExtinguishFire : MonoBehaviour
 {
     public ParticleSystem controllerWaterParticles;
     public ParticleSystem handWaterParticles;
-    public bool handTrackedMode;
     public ParticleSystem fireParticles;
     public int timeToExtinguish = 500;
     public AudioSource fireExtinguishingAudio;
     ParticleSystem currentWaterParticles;
     bool soundIsPlaying;
+
+    public bool handTrackedMode = false;
+
+    public struct userAttributes
+    {
+    }
+
+    public struct appAttributes
+    {
+    }
+
+    async Task InitializeRemoteConfigAsync()
+    {
+        // initialize handlers for unity game services
+        await UnityServices.InitializeAsync();
+
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+    }
+
+    async Task Awake()
+    {
+        // initialize Unity's authentication and core services
+        await InitializeRemoteConfigAsync();
+
+        // Add a listener to apply settings when successfully retrieved:
+        RemoteConfigService.Instance.FetchCompleted += ApplyRemoteConfig;
+        await RemoteConfigService.Instance.FetchConfigsAsync(new userAttributes(), new appAttributes());
+
+        RemoteConfigService.Instance.GetConfig("handTrackedMode");
+    }
+
+    void ApplyRemoteConfig(ConfigResponse configResponse)
+    {
+        // Conditionally update settings, depending on the response's origin:
+        switch (configResponse.requestOrigin)
+        {
+            case ConfigOrigin.Default:
+                Debug.Log("No settings loaded this session and no local cache file exists; using default values.");
+                break;
+            case ConfigOrigin.Cached:
+                Debug.Log("No settings loaded this session; using cached values from a previous session.");
+                break;
+            case ConfigOrigin.Remote:
+                Debug.Log("New settings loaded this session; update values accordingly.");
+                Debug.Log("handTrackedMode: " + RemoteConfigService.Instance.appConfig.GetBool("handTrackedMode"));
+                break;
+        }
+
+        handTrackedMode = RemoteConfigService.Instance.appConfig.GetBool("handTrackedMode");
+    }
 
     // Start is called before the first frame update
     void Start()
